cdae6aae825658f07c1b80133001d2a71fad4277
Matías Caro Serrano <caromatiasm@gmail.com>
2025-05-18 22:16:36 -0400

    Changes in paymentMethod for v3

--- src/Entity/PaymentMethod.php
+++ src/Entity/PaymentMethod.php
@@ -2,131 +2,74 @@
 
 namespace LaravelMercadoPago\Entity;
 
-use MercadoPago\Entity;
-use MercadoPago\Annotation\Attribute;
-use MercadoPago\Annotation\RestMethod;
+use MercadoPago\Client\PaymentMethod\PaymentMethodClient;
+use MercadoPago\MercadoPagoConfig;
 use LaravelMercadoPago\Traits\EntityTrait;
 use LaravelMercadoPago\Interfaces\ClassToJson;
 
 /**
- * paymentmethod class
- * @RestMethod(resource="/v1/payment_methods/search", method="search")
- * @RestMethod(resource="/v1/payment_methods", method="list")
+ * PaymentMethod class
  */
-class PaymentMethod extends Entity implements ClassToJson
+class PaymentMethod implements ClassToJson
 {
     use EntityTrait;
 
     /**
-     * id
-     * @Attribute()
-     * @var string
+     * Propiedades para almacenar los datos del método de pago
      */
     protected $id;
-
-    /**
-     * name
-     * @Attribute()
-     * @var string
-     */
     protected $name;
-
-
-    /**
-     * payment_type_id
-     * @Attribute()
-     * @var string
-     */
     protected $payment_type_id;
-
-
-    /**
-     * status
-     * @Attribute()
-     * @var string
-     */
     protected $status;
-
-    /**
-     * secure_thumbnail
-     * @Attribute()
-     * @var string
-     */
     protected $secure_thumbnail;
-
-    /**
-     * thumbnail
-     * @Attribute()
-     * @var string
-     */
     protected $thumbnail;
-
-    /**
-     * deferred_capture
-     * @Attribute()
-     * @var string
-     */
     protected $deferred_capture;
-
-    /**
-     * settings
-     * @Attribute()
-     * @var array
-     */
     protected $settings;
-
-    /**
-     * additional_info_needed
-     * @Attribute()
-     * @var array
-     */
     protected $additional_info_needed;
-
-
-    /**
-     * min_allowed_amount
-     * @Attribute()
-     * @var int
-     */
     protected $min_allowed_amount;
-
-    /**
-     * max_allowed_amount
-     * @Attribute()
-     * @var int
-     */
     protected $max_allowed_amount;
-
-    /**
-     * accreditation_time
-     * @Attribute()
-     * @var int
-     */
     protected $accreditation_time;
-
-
-    /**
-     * financial_institutions
-     * @Attribute()
-     * @var array
-     */
     protected $financial_institutions;
-
-    /**
-     * processing_modes
-     * @Attribute()
-     * @var array
-     */
     protected $processing_modes;
-
+    
     /**
-     * Consultar los medios de pago disponibles 
+     * Cliente de MercadoPago para métodos de pago
+     */
+    private $client;
+    
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->client = new PaymentMethodClient();
+    }
+    
+    /**
+     * Consultar los medios de pago disponibles (con filtros)
      * @link https://www.mercadopago.com.co/developers/es/reference/payment_methods/_payment_methods/get
      * @return array
      */
     public function findV2($filters = [])
     {
-        return static::search($filters);
+        try {
+            $response = $this->client->list();
+            
+            if (!empty($filters) && isset($filters['bins'])) {
+                $cardBin = $filters['bins'];
+                $filteredMethods = array_filter($response, function($method) use ($cardBin) {
+  
+                    return isset($method->settings) && 
+                           isset($method->settings->bin) && 
+                           strpos($method->settings->bin, substr($cardBin, 0, 6)) === 0;
+                });
+                return array_values($filteredMethods);
+            }
+            
+            return $response;
+        } catch (\Exception $e) {
+            return [];
+        }
     }
 
     /**
@@ -136,7 +79,11 @@ class PaymentMethod extends Entity implements ClassToJson
      */
     public function find()
     {
-        return static::all();
+        try {
+            return $this->client->list();
+        } catch (\Exception $e) {
+            return [];
+        }
     }
 
     /**
@@ -146,7 +93,44 @@ class PaymentMethod extends Entity implements ClassToJson
      */
     public function findCreditCard($cardNumber)
     {
-        $credict = $this->findV2(['bins' => $cardNumber ]);
-        return $credict[0];
+        $credit = $this->findV2(['bins' => $cardNumber]);
+        return !empty($credit) ? $credit[0] : null;
+    }
+    
+    /**
+     * Método estático para compatibilidad con código existente
+     * @return array
+     */
+    public static function all()
+    {
+        $instance = new self();
+        return $instance->find();
+    }
+    
+    /**
+     * Método estático para compatibilidad con código existente
+     * @param array $filters
+     * @return array
+     */
+    public static function search($filters = [])
+    {
+        $instance = new self();
+        return $instance->findV2($filters);
+    }
+    
+    /**
+     * Setter mágico para mantener compatibilidad
+     */
+    public function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+    
+    /**
+     * Getter mágico para mantener compatibilidad
+     */
+    public function __get($name)
+    {
+        return $this->$name ?? null;
     }
 }
